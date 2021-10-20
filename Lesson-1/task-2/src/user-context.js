6e3c042bbbb88da4d34c98388845954aad3ae975
Rmorhub <rmorphone@gmail.com>
2021-10-20 21:04:53 +0300

    task-2

--- Lesson-1/task-2/src/user-context.js
+++ Lesson-1/task-2/src/user-context.js
@@ -1,10 +1,3 @@
 import React from 'react';
 
-export const userDate = {
-  teslaUser: {
-    name: 'Nikola Tesla',
-    avatar_url: 'https://avatars3.githubusercontent.com/u10001',
-  },
-};
-
 export const UserContext = React.createContext({});
