07b5ddbd59b4ad13eae637e85c0856173a108de2
Rmorhub <rmorphone@gmail.com>
2021-10-20 20:18:23 +0300

    task-2

--- Lesson-1/task-2/src/App.jsx
+++ Lesson-1/task-2/src/App.jsx
@@ -4,16 +4,14 @@ import { userContext } from './userData-context';
 
 class App extends React.Component {
   state = {
-    userDate: {
-      name: 'Nikola Tesla',
-      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
-    },
+    name: 'Nikola Tesla',
+    avatar_url: 'https://avatars3.githubusercontent.com/u10001',
   };
 
   render() {
     return (
       <div className="page">
-        <userContext.Provider value={this.state.userDate}>
+        <userContext.Provider value={this.state}>
           <Header />
         </userContext.Provider>
       </div>
