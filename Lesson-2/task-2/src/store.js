3f92f95d2e04a1a45f439c6bca54b173c1557553
Rmorhub <rmorphone@gmail.com>
2021-10-21 19:29:15 +0300

    task-2

---
+++ Lesson-2/task-2/src/store.js
@@ -0,0 +1,50 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
+
+export const increment = () => ({
+  type: INCREMENT,
+});
+
+export const decrement = () => ({
+  type: DECREMENT,
+});
+
+export const reset = () => ({
+  type: RESET,
+});
+
+const initialState = {
+  history: [],
+  result: [],
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        history: state.history.concat('1'),
+        result: state.result.concat(1),
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        history: state.history.concat('-1'),
+        result: state.result.concat(-1),
+      };
+    case RESET:
+      return {
+        ...state,
+        history: [],
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
