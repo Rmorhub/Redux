2407a77be3324983852a2c436b298651b81221be
Rmorhub <rmorphone@gmail.com>
2021-10-22 15:19:30 +0300

    task-3

--- Lesson-2/task-3/src/index.js
+++ Lesson-2/task-3/src/index.js
@@ -1,5 +1,5 @@
 import store from './store';
-import { addUser, deleteUser } from './user.actions';
+import { addUser, deleteUser } from './users.actions';
 
 store.dispatch(addUser({ id: 76, name: 'Sarah' }));
 store.dispatch(addUser({ id: 10, name: 'Bob' }));
