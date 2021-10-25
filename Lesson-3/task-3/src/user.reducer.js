5b5d60bfa5e50495c9f80d644f0e3256a25c01f4
Rmorhub <rmorphone@gmail.com>
2021-10-25 13:20:48 +0300

    task-3

--- Lesson-3/task-3/src/user.reducer.js
+++ Lesson-3/task-3/src/user.reducer.js
@@ -3,9 +3,7 @@ import { SET_USER, REMOVE_USER } from './user.actions';
 export const userReducer = (state = null, action) => {
   switch (action.type) {
     case SET_USER: {
-      return {
-        user: action.payload.user,
-      };
+      return action.payload.user;
     }
     case REMOVE_USER: {
       return null;
